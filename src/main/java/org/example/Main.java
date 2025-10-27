0b791d7e4e0a693170bfbdf08d368c53b67a8b59
dario <brkicd@bzz.ch>
2025-10-27 14:06:40 +0100

    saveup #2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -31,61 +31,105 @@ public class Main {
             int choiceProgramm = input.nextInt();
 
             if (choiceProgramm == 1) {
-
-                System.out.print("Erste Arbeitsschicht (Von): ");
-                String ersteVon = input.next();
-                System.out.print("Erste Arbeitsschicht (Bis): ");
-                String ersteBis = input.next();
-                System.out.print("Zweite Arbeitsschicht (Von): ");
-                String zweiteVon = input.next();
-                System.out.print("Zweite Arbeitsschicht (Bis): ");
-                String zweiteBis = input.next();
-
-                double totalErsteSchicht = BerechnungSchicht1(ersteVon,ersteBis);
-
-                double totalZweiteSchicht = BerechnungSchicht2(zweiteVon,zweiteBis);
+                String ersteVon = "";
+                String ersteBis = "";
+                String zweiteVon = "";
+                String zweiteBis = "";
+                double totalErsteSchicht = 0;
+                double totalZweiteSchicht = 0;
 
 
+                boolean korrekt = false;
 
+                while (!korrekt) {
+                    System.out.print("Erste Arbeitsschicht (Von): ");
+                    ersteVon = input.next();
+                    System.out.print("Erste Arbeitsschicht (Bis): ");
+                    ersteBis = input.next();
+                    System.out.print("Zweite Arbeitsschicht (Von): ");
+                    zweiteVon = input.next();
+                    System.out.print("Zweite Arbeitsschicht (Bis): ");
+                    zweiteBis = input.next();
+
+                    totalErsteSchicht = BerechnungSchicht1(ersteVon, ersteBis);
+                    totalZweiteSchicht = BerechnungSchicht1(zweiteVon, zweiteBis);
+
+
+
+                    if (errorDetector(totalErsteSchicht, totalZweiteSchicht)) {
+                        System.out.println("❌ Ihre Eingabe entspricht nicht dem Reglement. Bitte erneut eingeben.\n");
+                    } else {
+                        korrekt = true;
+                    }
+                }
+
+                double totalDesTages = totalErsteSchicht + totalZweiteSchicht;
 
                 System.out.println(" ");
                 System.out.println(" ");
 
                 System.out.println("|--------------------------------------|-----------|-----------|-----------|");
-                System.out.println("|  Erste Arbeitsschicht  (Von - Bis):  |   " + ersteVon + "   |   " + ersteBis + "   |" + "          |");
+                System.out.println("|  Erste Arbeitsschicht  (Von - Bis):  |   " + ersteVon + "   |   " + ersteBis + "   |" + "    "+ totalErsteSchicht + "     ");
                 System.out.println("|--------------------------------------|-----------|-----------|-----------|");
-                System.out.println("|  Zweite Arbeitsschicht (Von - Bis):  |   " + zweiteVon + "   |   " + zweiteBis + "   |" + "          |");
+                System.out.println("|  Zweite Arbeitsschicht (Von - Bis):  |   " + zweiteVon + "   |   " + zweiteBis + "   |" + "    "+ totalZweiteSchicht + "       ");
                 System.out.println("|--------------------------------------|-----------|-----------|-----------|");
-                System.out.println("|  Totale Arbeitszeit: ");
+                System.out.println("|  Totale Arbeitszeit:                 |           |           |    " + totalDesTages);
+                System.out.println("|--------------------------------------|-----------|-----------|-----------|");
 
-                System.out.println(" ");
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                System.out.println(" Totalbericht des Arbeitstages:");
+                System.out.println(" Überstunden: ");
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
                 System.out.println(" ");
             }
             else {
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
                 System.out.println("Sie haben das Programm beendet.");
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
                 terminateProgramm();
             }
         }
     }
 
     public static double BerechnungSchicht1(String ersteVon, String ersteBis) {
-        return 0;
-    }
 
-    public static double BerechnungSchicht2(String zweiteVon, String zweiteBis) {
-        return 0;
+
+        String[] teile = ersteVon.split(":");
+        int stunden = Integer.parseInt(teile[0]);
+        int minuten = Integer.parseInt(teile[1]);
+
+        double dezimalErsteVon = stunden + (minuten / 60.0);
+
+        teile = ersteBis.split(":");
+        stunden = Integer.parseInt(teile[0]);
+        minuten = Integer.parseInt(teile[1]);
+
+        double dezimalErsteBis = stunden + (minuten / 60.0);
+
+        return dezimalErsteBis - dezimalErsteVon;
     }
 
     public static double BerechnungTotal() {
         return 0;
     }
 
-    public static double berechnungUeberstunden () {
+    public static double berechnungUeberstunden() {
         return 0;
     }
 
     public static void terminateProgramm() {
         terminate = true;
+    }
+
+    public static boolean errorDetector(double Schicht1, double Schicht2) {
+        double total = Schicht1 + Schicht2;
+
+        if (total > 9){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
 
